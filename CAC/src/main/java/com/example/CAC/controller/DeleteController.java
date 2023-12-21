a0be3592873417541cd0f9d82027493f3c214bff
KKKJJJSSS <kj971023@naver.com>
2023-12-22 02:42:00 +0900

    20231128 cloudtype -> AWS RDS DB 이전

--- CAC/src/main/java/com/example/CAC/controller/DeleteController.java
+++ CAC/src/main/java/com/example/CAC/controller/DeleteController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 @RequiredArgsConstructor
 @Controller
